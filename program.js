2deb09cbf05f18e32534bab0b88ac1c99f43df59
Magda Balun <magi@op.pl>
2018-01-03 01:04:31 +0100

    Add program.js file

---
+++ program.js
@@ -0,0 +1,25 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/version':
+                process.stdout.write('Node.js version - ' + process.versions.node + '.\n');
+                break;
+            case '/lang':
+                process.stdout.write('Language (macOS i Linux) - ' + process.env.lang + '.\n');
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+                break;
+        }
+    }
+});
